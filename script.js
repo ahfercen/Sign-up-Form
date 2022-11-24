c1bd76f9a8e5fb87ab7e172de033ab08b8c3d0f9
ahfercen <ahfercen@gmail.com>
2022-11-24 16:26:46 -0500

    JS checking if passwords match

--- script.js
+++ script.js
@@ -2,16 +2,19 @@ const pass1 = document.querySelector("#pass");
 const pass2 = document.querySelector("#confirm");
 const form = document.querySelector("form");
 
-console.log(pass1);
-console.log(pass2);
-console.log(form);
 
 function checkPass(){
-    if(pass1.textContent === pass2.textContent) {
+    if(pass1.value === pass2.value) {
         return true;
     }else{
-        console.log(pass1.textContent);
-        console.log(pass2.textContent);
+        return false;
     }
 }
-form.submit(checkPass());
+addEventListener('submit',(evt) => {
+        if(checkPass()){
+            console.log("paased");
+        }else{
+            evt.preventDefault();
+        }
+});
+
